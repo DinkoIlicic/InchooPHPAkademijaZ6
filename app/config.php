0fbf2230e5cae9c96d5f2fdfbe59efb21484daa7
DinkoIlicic <ilicicdinko@gmail.com>
2019-02-02 10:49:40 +0100

    test

---
+++ app/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'mode' => 'development',
+    'url' => 'http://polaznik18mvc.inchoo4u.net',
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'social_network',
+        'user' => 'root',
+        'password' => 'vinkovci123'
+    ]
+];
