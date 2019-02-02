c5c9294d870f0c2a1e2d44e55ed122c952848ec0
DinkoIlicic <ilicicdinko@gmail.com>
2019-02-02 09:45:05 +0100

    Created repository and added files

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+define('BP', __DIR__ . '/');
+
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
+$includePaths = implode(PATH_SEPARATOR, [
+    BP . 'app/model',
+    BP . 'app/controller',
+    BP . 'app/model/entity'
+]);
+
+set_include_path($includePaths);
+spl_autoload_register(function ($class)
+{
+    $class = strtr($class, '\\', DIRECTORY_SEPARATOR) . '.php';
+    $file = stream_resolve_include_path($class);
+    if($file){
+        include $file;
+        return true;
+    }
+    return false;
+
+});
+
+App::start();
